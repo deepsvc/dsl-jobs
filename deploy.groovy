93f36bdee7b5b553bb859014e498838742a8a71b
Deepak Choudhary <deepak@flux7.com>
2020-12-22 16:49:19 +0530

    updated groovy file

--- deploy.groovy
+++ deploy.groovy
@@ -1,21 +1,13 @@
-def jenkinsfile = "Jenkinsfile"
-pipelineJob("wade-deployment") {
-  parameters {
-  stringParam("REGION", "us-east-1", description="")
-  stringParam("BRANCH", "", description="")
-  stringParam("APPLICATION_NAME", "", description="")
-  choiceParam("ENV", ["dev", "test", "uat"], description="")
-  booleanParam("S3_BUCKETS", false, "")
-  booleanParam("LAMBDA_LAYERS", false, "")
-  booleanParam("DDB_TABLE", false, "")
-  booleanParam("PINPOINT", false, "")
-  booleanParam("USER_POOL", false, "")
-  booleanParam("LAMBDA_STACK", false, "")
-  booleanParam("API", true, "")
-  booleanParam("WAF", false, "")
-  booleanParam("CLOUDFRONT", false, "")
-  
-}
+/* groovylint-disable LineLength */
+def jenkinsfile = 'Jenkinsfile'
+pipelineJob('ansible-job') {
+    parameters {
+        stringParam('Docker_Image_Name', 'nginx-app', description = 'Name will be used for Docker Image Deployed by Terraform')
+        stringParam('TF_Backend_Config_S3_Bucket_Name', 'test-bucket-terraform-project', description = 'S3 Bucket Name where TF State File will be Stored')
+        stringParam('TF_Backend_Config_Key', 'network/terraform.tfstate', description = 'Path in the S3 Bucket where TF State will be Stored')
+        stringParam('TF_Backend_Config_Region', 'ap-south-1', description = 'Region of the S3 Bucket')
+        choiceParam('Terraform_Action', ['Init', 'Plan', 'Deploy_Planned', 'Deploy_Directly', 'Destroy'], description = 'Terraform Actions you want to perform')
+    }
     logRotator(numToKeep = 100)
     definition {
         cps {
